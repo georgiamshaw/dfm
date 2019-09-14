e71249e5498cc6b3bef7ce5d0bcd88797eae4c9e
Victor Masson <victormasson21@gmail.com>
2019-09-14 18:23:44 +0100

    Displays article based on slider value

--- public/js/index.js
+++ public/js/index.js
@@ -2,15 +2,21 @@
 const sliderButton = document.querySelector('.slider-input');
 const sliderValue = document.querySelector('.slider-value');
 
+// DISPLAY RELEVANT ARTICLE BASED ON SLIDER VALUE
 sliderButton.addEventListener('change', (event) => {
-  console.log(event.target.value);
+  let containers = document.querySelectorAll('.article-container');
+  containers.forEach(article => {article.style.display = 'none'});
   sliderValue.innerText = event.target.value;
+  document.getElementById(`${event.target.value}`).setAttribute('style', 'display:block');
 })
 
-
+// PULL DATA FROM FRONT END
 const dataUrl = '../../src/data.json'
+let parsedData = {};
+
 window.onload = () => {
   console.log('Window loaded');
+
   // fetch(dataUrl)
   //   .then(data => {
   //     console.log('fetch runs');
@@ -18,12 +24,11 @@ window.onload = () => {
   //   })
   //   .then(data => console.log(data))
   //   .catch(err => console.log(err))
+
 }
 
+// URL TRIMMER
 const articleUrl = 'https://www.theguardian.com/politics/2019/sep/13/david-cameron-accuses-boris-johnson-and-michael-gove-of-behaving-appallingly-over-brexit'
-
 const urlTrimmer = (url) => {
   return url.split('//')[1].split('/')[0];
 }
-
-console.log(urlTrimmer(articleUrl));
