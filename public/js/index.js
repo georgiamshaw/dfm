16d8c067ab19399be07e1fbc49a4df08f70ad021
georgia.shaw <georgia@cranbrookw4.com>
2019-09-15 12:05:50 +0100

    add anchor tag to search button

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,14 @@
+const sliderButton = document.querySelector(".slider-input");
+const sliderValue = document.querySelector(".slider-value");
 
-const sliderButton = document.querySelector('.slider-input');
-const sliderValue = document.querySelector('.slider-value');
-
-sliderButton.addEventListener('change', (event) => {
+sliderButton.addEventListener("change", event => {
   console.log(event.target.value);
   sliderValue.innerText = event.target.value;
-})
+});
 
-
-const dataUrl = '../../src/data.json'
+const dataUrl = "../../src/data.json";
 window.onload = () => {
-  console.log('Window loaded');
+  console.log("Window loaded");
   // fetch(dataUrl)
   //   .then(data => {
   //     console.log('fetch runs');
@@ -18,12 +16,13 @@ window.onload = () => {
   //   })
   //   .then(data => console.log(data))
   //   .catch(err => console.log(err))
-}
+};
 
-const articleUrl = 'https://www.theguardian.com/politics/2019/sep/13/david-cameron-accuses-boris-johnson-and-michael-gove-of-behaving-appallingly-over-brexit'
+const articleUrl =
+  "https://www.theguardian.com/politics/2019/sep/13/david-cameron-accuses-boris-johnson-and-michael-gove-of-behaving-appallingly-over-brexit";
 
-const urlTrimmer = (url) => {
-  return url.split('//')[1].split('/')[0];
-}
+const urlTrimmer = url => {
+  return url.split("//")[1].split("/")[0];
+};
 
 console.log(urlTrimmer(articleUrl));
---
+++ public/js/search.js
@@ -0,0 +1,8 @@
+// const searchInput = document.querySelector("#search-input");
+// const submitButton = document.querySelector(".search-button");
+//
+// const showResults = e => {
+//   e.preventDefault();
+// };
+//
+// submitButton.addEventListener("click", showResults);
